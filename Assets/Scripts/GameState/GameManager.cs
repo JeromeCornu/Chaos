041ac64738623bc5900da72eb37b912b3e962eee
nyvle <elvynwakeford@gmail.com>
2025-05-08 00:26:56 +0200

    Should now only launch the pregame phase when all players have connected

--- Assets/Scripts/GameState/GameManager.cs
+++ Assets/Scripts/GameState/GameManager.cs
@@ -1,8 +1,6 @@
-using System;
+using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using Mirror;
-using UnityEngine;
 
 namespace GameState
 {
@@ -11,29 +9,64 @@ namespace GameState
         private EGameStates _gameState;
         private Dictionary<EGameStates, GameState> _gameStates = new ();
         List<PlayerMovementController> _players;
+        
+        public static GameManager Instance;
         public GameState GetGameState => _gameStates[_gameState];
 
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+        
         private void Init()
         {
             _gameStates[EGameStates.Fight] = new FightState();
             _gameStates[EGameStates.CardChoose] = new ChoosePowerUp();
             _gameStates[EGameStates.MapEditing] = new FightState();
         }
+
+        public override void OnStartClient()
+        {
+            base.OnStartClient();
+            if (isLocalPlayer)
+            {
+                StartCoroutine(NotifyServerReady());
+            }
+        }
         
-        [ClientRpc]
-        public void RpcChangeState(EGameStates state)
+        public void StartStateMachine()
+        {
+            Init();
+            if(isServer)
+                RpcChangeState(EGameStates.PreGame);
+        }
+        
+        private IEnumerator NotifyServerReady()
+        {
+            yield return null;
+            CmdPlayerSceneReady();
+        }
+        
+        [Command]
+        private void CmdPlayerSceneReady()
+        {
+            GameSync.Instance.PlayerReady(connectionToClient);
+        }
+                
+        [ClientRpc] 
+        private void RpcChangeState(EGameStates state) 
         {
             _gameStates[_gameState].Disable();
             _gameState = state;
             _gameStates[_gameState].Enable();
         }
-        private void OnEnable()
-        {
-            Init();
-            
-            RpcChangeState(EGameStates.Fight);
-        }
-        
         void Update()
         {
             _gameStates[_gameState].OnUpdate();
