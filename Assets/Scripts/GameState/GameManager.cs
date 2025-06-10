68952d8d1e14a7d959a2400c27fb1399c870bd74
nyvle <elvynwakeford@gmail.com>
2025-06-10 17:06:04 +0200

    added debug log

--- Assets/Scripts/GameState/GameManager.cs
+++ Assets/Scripts/GameState/GameManager.cs
@@ -173,6 +173,7 @@ namespace GameState
 
         public void MoveCardCursor(int direction)
         {
+            Debug.Log($"[GameManager] Moved {direction}");
             if (cardCount == 0 && CardNavigationUI.Instance != null)
                 cardCount = CardNavigationUI.Instance.TotalCards;
 
